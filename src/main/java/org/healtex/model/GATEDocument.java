7031a8274f0f96ebb377865ffcc1499e27811774
Kenneth Lui <k.lui@ucl.ac.uk>
2016-12-22 17:12:35 +0000

    Introduce GATEDocument model

---
+++ src/main/java/org/healtex/model/GATEDocument.java
@@ -0,0 +1,52 @@
+package org.healtex.model;
+
+import java.util.List;
+
+public class GATEDocument {
+    private String fileName;
+    private String personId;
+    private String perPersonDocumentId;
+    private List<String> namedEntities;
+
+    public GATEDocument() {
+
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPerPersonDocumentId(String perPersonDocumentId) {
+        this.perPersonDocumentId = perPersonDocumentId;
+    }
+
+    public String getPerPersonDocumentId() {
+        return perPersonDocumentId;
+    }
+
+    public void setNamedEntities(List<String> namedEntities) {
+        this.namedEntities = namedEntities;
+    }
+
+    public List<String> getNamedEntities() {
+        return namedEntities;
+    }
+
+    @Override
+    public String toString() {
+        return fileName;
+    }
+
+}
