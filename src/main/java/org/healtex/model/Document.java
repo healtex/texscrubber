adb1fcd27602a0add7739b31552ebd89b9d2b71c
Kenneth Lui <k.lui@ucl.ac.uk>
2016-12-22 16:58:50 +0000

    Update Document model

--- src/main/java/org/healtex/model/Document.java
+++ src/main/java/org/healtex/model/Document.java
@@ -4,9 +4,18 @@ public class Document {
     private String fileName;
     private String personId;
     private String perPersonDocumentId;
+    private String content;
 
     public Document() {
 
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
     }
 
     public void setFileName(String fileName) {
@@ -15,6 +24,22 @@ public class Document {
 
     public String getFileName() {
         return fileName;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPerPersonDocumentId(String perPersonDocumentId) {
+        this.perPersonDocumentId = perPersonDocumentId;
+    }
+
+    public String getPerPersonDocumentId() {
+        return perPersonDocumentId;
     }
 
     @Override
