b959feedd0834081cddac4f76b39862eb7b709f4
Kenneth Lui <k.lui@ucl.ac.uk>
2016-12-22 16:35:50 +0000

    Clean up JobCompletionNotificationListener

--- src/main/java/org/healtex/batch/listener/JobCompletionNotificationListener.java
+++ src/main/java/org/healtex/batch/listener/JobCompletionNotificationListener.java
@@ -6,19 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
-
-    @Autowired
-    public JobCompletionNotificationListener() {
-    }
 
     @Override
     public void afterJob(JobExecution jobExecution) {
